6f2b9db69e0cfec69db154870d41409521be84ab
Leandro Souza <sl.leandro09@gmail.com>
2024-01-30 20:40:45 -0300

    Design inicial da HomePage

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -5,8 +5,59 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home Page'),
+    return Container(
+      color: Colors.blueAccent,
+      constraints: const BoxConstraints.expand(),
+      child: const SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(32.0),
+                    child: Text(
+                      'São Paulo, SP - Brazil',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 32.0,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Icon(
+                      Icons.sunny,
+                      color: Colors.amberAccent,
+                      size: 164.0,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(32.0),
+                    child: Text(
+                      '27° C',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 28.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            FloatingActionButton(
+              onPressed: null,
+              backgroundColor: Colors.white,
+              child: Icon(Icons.search),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
