2091fa110babdf1e537e3d0b2de83f8fe66ec312
Leandro Souza <sl.leandro09@gmail.com>
2024-02-27 21:23:52 -0300

    Classes para cidades

---
+++ lib/models/city.dart
@@ -0,0 +1,13 @@
+class City {
+  String? name;
+  String? state;
+  String? country;
+  double? temperature;
+
+  City({
+    this.name,
+    this.state,
+    this.country,
+    this.temperature,
+  });
+}
---
+++ lib/repositories/city_repository.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:weather_app_flutter/models/city.dart';
+
+class CityRepository {
+  final List<City> _cities = [
+    City(
+      name: "Belo Horizonte",
+      state: "Minas Gerais",
+      country: "Brazil",
+      temperature: 23.0,
+    ),
+    City(
+      name: "Rio de Janeiro",
+      state: "Rio de Janeiro",
+      country: "Brazil",
+      temperature: 34.0,
+    ),
+    City(
+      name: "Porto Alegre",
+      state: "Rio Grande do Sul",
+      country: "Brazil",
+      temperature: 18.0,
+    ),
+  ];
+
+  Future<dynamic> getCityByName(String name) async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    return _cities.firstWhere((city) => city.name == name);
+  }
+}
