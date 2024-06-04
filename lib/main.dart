7332028c2387fd6729d1c1515a488bb5ab55e504
Leandro Souza <sl.leandro09@gmail.com>
2024-06-04 20:31:55 -0300

    Colocando o CityViewModel na arvore de widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import './viewmodels/city_view_model.dart';
 import './screens/home_page.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => CityViewModel(),
+      child: const MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
