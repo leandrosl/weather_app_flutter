e5665b4d3ed4d675d617ff303b21eb945b28b6fe
Leandro Souza <sl.leandro09@gmail.com>
2025-01-28 20:48:10 -0300

    Arquivo para armazenar os temas do app

---
+++ lib/themes.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+final weatherAppDarkTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.blue,
+    brightness: Brightness.dark,
+  ),
+);
+
+final weatherAppLightTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.blue,
+    brightness: Brightness.light,
+  ),
+);
